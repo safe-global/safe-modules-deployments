64f74d753713322ca769f1d5773549349ec80fc3
Nicholas Rodrigues Lordello <nick@safe.global>
2023-12-11 02:39:59 -0500

    Add Module v0.2.0 Deployment (#12)
    This PR adds the new v0.2.0 canonical 4337 module deployment.
    
    Note that we additionally **removed the old v0.1.0 deployment** because
    of a known medium severity issue with the module.

--- src/safe-4337-module.ts
+++ src/safe-4337-module.ts
@@ -1,11 +1,11 @@
-import Safe4337Module010 from './assets/safe-4337-module/v0.1.0/safe-4337-module.json';
-import AddModulesLib010 from './assets/safe-4337-module/v0.1.0/add-modules-lib.json';
+import Safe4337Module020 from './assets/safe-4337-module/v0.2.0/safe-4337-module.json';
+import AddModulesLib020 from './assets/safe-4337-module/v0.2.0/add-modules-lib.json';
 import { DeploymentFilter, Deployment } from './types';
 import { applyFilterDefaults, findDeployment } from './utils';
 
 // The array should be sorted from the latest version to the oldest.
-const SAFE_4337_MODULE_DEPLOYMENTS: Deployment[] = [Safe4337Module010];
-const ADD_MODULES_LIB_DEPLOYMENTS: Deployment[] = [AddModulesLib010];
+const SAFE_4337_MODULE_DEPLOYMENTS: Deployment[] = [Safe4337Module020];
+const ADD_MODULES_LIB_DEPLOYMENTS: Deployment[] = [AddModulesLib020];
 
 export const getSafe4337ModuleDeployment = (filter?: DeploymentFilter): Deployment | undefined => {
   return findDeployment(applyFilterDefaults(filter), SAFE_4337_MODULE_DEPLOYMENTS);
