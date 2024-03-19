1afb870634f11f9306808cae10b4819483e2a94f
Nicholas Rodrigues Lordello <nick@safe.global>
2024-03-19 10:26:25 +0100

    Safe 4337 Module v0.3.0 Deployments (#16)
    This PR adds deployments for the Safe 4337 module v0.3.0.
    
    We notably implement some backwards compatibility logic for the
    `SafeModuleSetup` contract which was renamed from `AddModulesLib`.

--- src/safe-4337-module.ts
+++ src/safe-4337-module.ts
@@ -1,16 +1,23 @@
+import Safe4337Module030 from './assets/safe-4337-module/v0.3.0/safe-4337-module.json';
 import Safe4337Module020 from './assets/safe-4337-module/v0.2.0/safe-4337-module.json';
+import SafeModuleSetup030 from './assets/safe-4337-module/v0.3.0/safe-setup-module.json';
 import AddModulesLib020 from './assets/safe-4337-module/v0.2.0/add-modules-lib.json';
 import { DeploymentFilter, Deployment } from './types';
 import { applyFilterDefaults, findDeployment } from './utils';
 
 // The array should be sorted from the latest version to the oldest.
-const SAFE_4337_MODULE_DEPLOYMENTS: Deployment[] = [Safe4337Module020];
-const ADD_MODULES_LIB_DEPLOYMENTS: Deployment[] = [AddModulesLib020];
+const SAFE_4337_MODULE_DEPLOYMENTS: Deployment[] = [Safe4337Module030, Safe4337Module020];
+const SAFE_MODULE_SETUP_DEPLOYMENTS: Deployment[] = [SafeModuleSetup030, AddModulesLib020];
 
 export const getSafe4337ModuleDeployment = (filter?: DeploymentFilter): Deployment | undefined => {
   return findDeployment(applyFilterDefaults(filter), SAFE_4337_MODULE_DEPLOYMENTS);
 };
 
-export const getAddModulesLibDeployment = (filter?: DeploymentFilter): Deployment | undefined => {
-  return findDeployment(applyFilterDefaults(filter), ADD_MODULES_LIB_DEPLOYMENTS);
+export const getSafeModuleSetupDeployment = (filter?: DeploymentFilter): Deployment | undefined => {
+  return findDeployment(applyFilterDefaults(filter), SAFE_MODULE_SETUP_DEPLOYMENTS);
 };
+
+// From v0.2 to v0.3, the `AddModulesLib` contract was renamed to `SafeModuleSetup` while preserving
+// its interface and functionality. As such, we consider both contracts to be the same with respect
+// to deployments, and expose an alias for the `AddModulesLib` name for backwards compatibility.
+export const getAddModulesLibDeployment = getSafeModuleSetupDeployment;
